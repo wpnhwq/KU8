32d9fa5bff9037bc23dbcc3086ade0604237bd90
painan <112233@qq.com>
2020-08-30 23:19:59 +0800

    tuinao

--- PathWithinrepositioyMavenXMu/AetiID/src/main/java/com/wpn/Demo.java
+++ PathWithinrepositioyMavenXMu/AetiID/src/main/java/com/wpn/Demo.java
@@ -5,6 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("main"); //dev
+		
+		
+		System.out.println("main"); //master
 	}
 
 }
